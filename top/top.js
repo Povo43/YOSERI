004fc918f3757ce29ce35b27bc510b4c5ee89961
Povo43 <140213985+Povo43@users.noreply.github.com>
2024-05-06 21:52:24 +0000

    fdfgdgf

--- top/top.js
+++ top/top.js
@@ -1,1 +1,19 @@
-var btn = document.getElementById('');
+/*タイマー*/
+const START = document.getElementById('timer-start');
+const STOP = document.getElementById('timer-stop');
+const RESET = document.getElementById('timer-reset');
+const HOURS = document.querySelector('input[name="timer-s"]');
+
+START.addEventListener('click', () => {
+    //クリックイベント
+    alert("スタートしました")
+    HOURS.value = "2";
+});
+
+STOP.addEventListener('click', () => {
+    alert("ストップしました")
+});
+
+RESET.addEventListener('click', () => {
+    alert("リセットしました")
+});
